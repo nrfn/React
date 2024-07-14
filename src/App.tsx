38a5b2ee826c9139595471f83ec161ddec249814
nrfn <59220732+nrfn@users.noreply.github.com>
2024-07-14 22:53:05 +0900

    App.tsxを修正。

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>React学習用</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +19,4 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+};
